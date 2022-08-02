0814a85a674e23a05cd734ad78ca60b797ef0571
Nidhi Sahu <Nidhi.Sahu@securemeters.com>
2022-08-02 10:14:04 +0530

    first commit

---
+++ DataFromViewToController/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataFromViewToController.Models;
+
+namespace DataFromViewToController.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //1 way to get data from view
+        [HttpPost]
+        public string PostUsingParameters(string firstName, string lastName)
+        {
+            return "from parameters - " + firstName + "," + lastName;
+        }
+        //second way to get data of view
+        [HttpPost]
+        public string PostUsingRequest()
+        {
+            string firstName = Request["firstName"];
+            string lastName = Request["lastName"];
+            return "From request -" + firstName + "," + lastName;
+        }
+        //3 way to get data of view
+        [HttpPost]
+        public string PostUsingFormCollection(FormCollection form)
+        {
+            string firstName = form["firstName"];
+            string lastName = form["lastName"];
+            return "From form collection-" + firstName + "," + lastName;
+        }
+        //4 way to get data of view
+        [HttpPost]
+        public ActionResult PostUsingBinding(Employee emp)
+        {
+            if(ModelState.IsValid)
+            {
+                return View();
+
+            }
+            return View("index");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DataFromViewToController/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //for validations
+
+namespace DataFromViewToController.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Please enter your first name")]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email{ get; set; }
+        [Required]
+        [Range(0,10)]
+        public int Age { get; set; }
+    }
+}
